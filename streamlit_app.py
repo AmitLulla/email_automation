77c8201e469df32e9b832415098fba2a7d00c70a
Amit Lulla <113911262+AmitLulla@users.noreply.github.com>
2024-02-23 11:32:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,34 +4,44 @@ import pandas as pd
 
 def main():
     st.title("Email Automation")
+    col1, col2 = st.columns(2)
+    with col1:
+        userId = st.text_input("Enter Gmail ID:")
+    with col2:        
+        userPass = st.text_input("Enter Gmail Password:", type = "password")
 
-    uploaded_file = st.file_uploader("Upload File", type=["xlsx", "csv"])
-    
-    if uploaded_file is not None:
-        try:
-            if uploaded_file.name.endswith('xlsx'):
-                xls = pd.ExcelFile(uploaded_file)
-                sheet_names = xls.sheet_names
-                selected_sheet = st.selectbox("Select a worksheet:", sheet_names)
-                df = pd.read_excel(xls, sheet_name=selected_sheet)
-            else:
-                df = pd.read_csv(uploaded_file)
-            email_ids = st.text_input("Map Email Ids")
-            email_subject = st.text_input("Email Subject")
-            email_template = st.text_area("Email Template")
-            headers = [f"{{{header}}}" for header in df.columns.tolist()]
-            st.write("File Binders:")
-            st.write(headers)
-            if not email_template:
-                st.warning("Please provide an email template.")
+    if userPass and userId:
+        uploaded_file = st.file_uploader("Upload File", type=["xlsx", "csv"])
+        
+        if uploaded_file:
+            try:
+                if uploaded_file.name.endswith('xlsx'):
+                    xls = pd.ExcelFile(uploaded_file)
+                    sheet_names = xls.sheet_names
+                    selected_sheet = st.selectbox("Select a worksheet:", sheet_names)
+                    df = pd.read_excel(xls, sheet_name=selected_sheet)
+                else:
+                    df = pd.read_csv(uploaded_file)
                 
-            if st.button("Send Emails"):
-                send_emails(df, email_subject, email_template, email_ids)
-                st.success("Emails sent successfully!")
-        except Exception as e:
-            st.error(f"Error: {e}")
+                email_subject = st.text_input("Email Subject")
+                email_list = st.text_input("Enter Receipetients")
+                email_template = st.text_area("Email Template")
+                headers = [f"{{{header}}}" for header in df.columns.tolist()]
+                st.write("File Binders:")
+                st.write(headers)
+                
+                if not email_template:
+                    st.warning("Please provide an email template.")
+                    
+                if st.button("Send Emails"):
+                    send_emails(df, email_subject, email_template, email_list, userId, userPass)
+                    st.success("Emails sent successfully!")
+            except Exception as e:
+                st.error(f"Error: {e}")
+        else:
+            st.warning("Please upload a file first.")
     else:
-        st.warning("Please upload a file first.")
+        st.warning("Please enter your login credentials first.")
 
 if __name__ == "__main__":
     main()
