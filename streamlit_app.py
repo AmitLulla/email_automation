32303b62972adb51a12acc3f2adfc81f0dfc2e41
Amit Lulla <113911262+AmitLulla@users.noreply.github.com>
2024-02-23 12:28:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
+import chromedriver_autoinstaller
 from webdriver_manager.chrome import ChromeDriverManager
 
 
